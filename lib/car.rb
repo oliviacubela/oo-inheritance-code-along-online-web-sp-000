8d29ebfcab2f99f03c268cd2ea232825edecab72
Olivia Cubela <oliviacubela@gmail.com>
2020-07-15 18:27:17 +0000

    Automatically backed up by Learn

--- lib/car.rb
+++ lib/car.rb
@@ -6,3 +6,7 @@ class Car < Vehicle
     "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
   end
 end
+#when your program is being executed, at the point at which the 'go'method is invoked, the compiler first looks 
+#in the class to which the instance of car that we are calling the method on belongs.
+#if it finds a 'go' method there, it will execute that method. If it doesn't find such a method there, 
+#it will move on to look in the parent class that this class inherits from.
