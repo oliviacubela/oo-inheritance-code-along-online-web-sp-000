868c1c91aeb240ce30c366620600d75260cbd831
Olivia Cubela <oliviacubela@gmail.com>
2020-07-15 18:24:05 +0000

    Automatically backed up by Learn

--- lib/car.rb
+++ lib/car.rb
@@ -1,1 +1,6 @@
 require_relative "./vehicle.rb"
+#we require "./vehicle.rb" because our Car class will need access to our Vehicle class, and therefore needs
+#access to the file that contains that class
+class Car < Vehicle
+
+end
